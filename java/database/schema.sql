e4de56a483f69ca18b881f598dbde3b89728f01b
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-05-19 14:23:48 -0400

    Database creation

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,15 +1,96 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users_ingredients, users_recipes, users, ingredients, recipes, meal_plan, 
-meal_plan_recipes, users_meal_plan, grocery_list,recipe_ingredients CASCADE;
+DROP TABLE IF EXISTS groups_player, groups_events, requests, user_event, match_user, users, groups, events, match CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
+	email varchar(100) NOT NULL,
+	city varchar(100) NOT NULL,
 	role varchar(50) NOT NULL,
 	
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+CREATE TABLE groups (
+	group_id serial,
+	group_name varchar (250) NOT NULL,
+	city varchar (50) NOT NULL,
+	location varchar (250) NULL,
+	created_by int NOT NULL,
+	
+	CONSTRAINT PK_groups PRIMARY KEY (group_id)
+);
+
+CREATE TABLE groups_player (
+	group_id int NOT NULL,
+	user_id int NOT NULL,
+	role varchar (50) NOT NULL,
+	
+	CONSTRAINT PK_groups_player PRIMARY KEY (group_id, user_id),
+	CONSTRAINT FK_groups_player_group_id FOREIGN KEY (group_id) REFERENCES groups(group_id),
+	CONSTRAINT FK_groups_player_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE events (
+	event_id serial,
+	event_name varchar(250) NOT NULL,
+	description varchar(300) NOT NULL,
+	event_date date NOT NULL,
+	start_time time NOT NULL,
+	end_time time NULL,
+	location varchar (250) NOT NULL,
+	created_by int NOT NULL,
+	
+	CONSTRAINT PK_events PRIMARY KEY (event_id)
+);
+CREATE TABLE groups_events(
+	group_id int NOT NULL,
+	event_id int NOT NULL,
+	
+	CONSTRAINT PK_groups_event PRIMARY KEY (group_id, event_id),
+	CONSTRAINT FK_groups_event_group_id FOREIGN KEY (group_id) REFERENCES groups(group_id),
+	CONSTRAINT FK_groups_event_event_id FOREIGN KEY (event_id) REFERENCES events (event_id)
+);
+
+CREATE TABLE requests(
+	request_id serial,
+	group_id int NOT NULL,
+	requester_id int NOT NULL,
+	status boolean NOT NULL,
+	
+	CONSTRAINT PK_requests PRIMARY KEY (request_id),
+	CONSTRAINT FK_requests_group_id FOREIGN KEY (group_id) REFERENCES groups (group_id),
+	CONSTRAINT FK_requests_requester_id FOREIGN KEY (requester_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE user_event (
+	user_id int NOT NULL,
+	event_id int NOT NULL,
+	
+	CONSTRAINT PK_user_event PRIMARY KEY (user_id, event_id),
+	CONSTRAINT FK_user_event_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_user_event_event_id FOREIGN KEY (event_id) REFERENCES events (event_id)
+);
+
+CREATE TABLE match (
+	match_id serial,
+	event_id int NOT NULL,
+	score varchar(50),
+	winner int,
+	winner_two int,
+	
+	CONSTRAINT PK_match PRIMARY KEY (match_id),
+	CONSTRAINT FK_match_event_id FOREIGN KEY (event_id) REFERENCES events (event_id)
+);
+
+CREATE TABLE match_user (
+	user_id int NOT NULL,
+	match_id int NOT NULL,
+	
+	CONSTRAINT PK_match_user PRIMARY KEY (user_id, match_id),
+	CONSTRAINT FK_match_user_user_id FOREIGN KEY (user_id) REFERENCES users (user_id),
+	CONSTRAINT FK_match_user_match_id FOREIGN KEY (match_id) REFERENCES match (match_id)
 );
 
 COMMIT TRANSACTION;
