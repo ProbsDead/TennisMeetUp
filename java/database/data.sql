95f6aadbd695ffa58746851c572abc24a6784612
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-05-22 13:24:47 -0400

    DB changes

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,9 +1,9 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users(username, password_hash, email, city, role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','miranda.lieblein@gmail.com', 
-																	  'Burlington','ROLE_USER');
-INSERT INTO users(username, password_hash, email, city, role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','tennis@aol.com',
-																	  'Chicago', 'ROLE_USER');
+INSERT INTO users(username, password_hash, first_name, last_name, email, city, role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'miranda', 
+																							 'lieblein', 'miranda.lieblein@gmail.com', 'Burlington','ROLE_USER');
+INSERT INTO users(username, password_hash, first_name, last_name, email, city, role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'tennis', 
+																							 'player','tennis@aol.com','Chicago', 'ROLE_USER');
 
 INSERT INTO groups(group_name, created_by, city, location, is_public) VALUES ('Volley Girls', 1, 'Burlington', '45 Tennis Ct, South Burlington, VT 05403', false);
 INSERT INTO groups(group_name, created_by, city, location, is_public) VALUES ('Chicago LOVE', 2, 'Chicago', '123 Main Ave, Chicago, IL 56792', true);
