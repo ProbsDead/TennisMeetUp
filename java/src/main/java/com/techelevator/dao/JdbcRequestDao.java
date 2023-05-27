403675500316f820e8a75b4645bb5cbab04e5805
Kate Kim <bkkatekim1@gmail.com>
2023-05-27 16:54:35 -0400

    resolving bean injection error

--- java/src/main/java/com/techelevator/dao/JdbcRequestDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcRequestDao.java
@@ -2,9 +2,11 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Request;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class JdbcRequestDao implements RequestDao{
     private JdbcTemplate jdbcTemplate;
 
