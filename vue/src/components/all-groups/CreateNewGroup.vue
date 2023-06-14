856b8dd0f4268e07841633907b0d1fd2a4b1a880
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-06-14 13:56:21 -0400

    Bugging google maps api call

--- vue/src/components/all-groups/CreateNewGroup.vue
+++ vue/src/components/all-groups/CreateNewGroup.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <form action="">
-            <label for="groupName">Group Name: </label>
+            <!-- <label for="groupName">Group Name: </label>
             <input type="text" id="groupName">
             
             <label for="state">State: </label>
@@ -16,7 +16,16 @@
             <label for="city">City: </label>
             <input type="text" id="city">
             <label for="location">Meet Up Location: </label>
-            <input type="text" id="autocomplete" placeholder="Address" ref="origin"/>
+            <input type="text" id="autocomplete" placeholder="Address" ref="origin"/> -->
+
+            <div>
+                <input type="text" name="location" id="location" v-model="location">
+                <ul>
+                    <li v-for="(result, i) in searchResults" :key="i">
+                        {{ result }} //list of places
+                    </li>
+                </ul>
+            </div>
            
         </form>
 
@@ -33,26 +42,51 @@ export default {
 
     data() {
         return{
-
+            location: '',
+            searchResults: [],
+            service: null //will reveal this later
+        }
+    },
+    metaInfo(){
+        return {
+            script: [{
+                src: `https://maps.googleapis.com/maps/api/js?key=AIzaSyB4VHlZLaqtdPglGyfl0Uk2-AHDeeuJBfU&libraries=places`,
+                async: true,
+                defer: true,
+                callback: () => this.MapsInit() //will declare it in methods
+            }]
         }
     },
 
     created() {
 
     },
-    mounted(){
-        const google = window.google;
-        const originAutocomplete = new google.maps.places.Autocomplete(
-            this.$refs["origin"],    
-        );
+    // mounted(){
+    //     const google = window.google;
+    //     const originAutocomplete = new google.maps.places.Autocomplete(
+    //         this.$refs["origin"], 
+    //         {
+    //             bounds: new google
+    //         }   
+    //     );
 
-        originAutocomplete.addListener("place_changed", () => {
-            originAutocomplete.getPlace();
-        });
+    //     originAutocomplete.addListener("place_changed", () => {
+    //         originAutocomplete.getPlace();
+    //     });
    
-    },
+    // },
 
     methods: {
+        MapsInit () {
+            this.service = new window.google.maps.places.AutocompleteService()
+        },
+        displaySuggestions (predictions, status) {
+            if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+                this.searchResults = []
+                return
+            }
+            this.searchResults = predictions.map(prediction => prediction.description)
+        },
 
         handleError(error){
       //A reusable error function to be used in the catch statements
@@ -62,6 +96,17 @@ export default {
         this.errorMsg = "An error occurred, please try again later.";
       }
     }
+    },
+    watch: {
+        location (newValue) {
+            if (newValue) {
+                this.service.getPlacePredictions({
+                    componentRestrictions: { country: 'us'},
+                    input: this.location,
+                    types: ['(address)']
+                }, this.displaySuggestions)
+            }
+        }
     }
     
 }
