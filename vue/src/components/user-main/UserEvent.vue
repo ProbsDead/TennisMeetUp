5fb9255ad7b981847cfc698cbd5388c00e367d7a
Kate Kim <bkkatekim1@gmail.com>
2023-06-22 19:10:41 -0400

    displaying event details

--- vue/src/components/user-main/UserEvent.vue
+++ vue/src/components/user-main/UserEvent.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    {{eventInfo.event_name}}
+    <h2> {{formattedDate}}</h2>
+    <hr>
+
+    <div>{{eventInfo.event_name}}</div>
+    <div>{{eventInfo.description}}</div>
+    <div>{{eventInfo.start_time}}</div>
+    <div>{{eventInfo.end_time}}</div>
+    <div>{{location}}</div>
+    
   </div>
 </template>
 
@@ -8,7 +16,23 @@
 export default {
   props: {
     eventInfo: Object
-  }
+  },
+  computed: {
+    formattedDate() {
+    const dateObject = new Date(this.eventInfo.start_time);
+    const month = dateObject.getMonth() +1;
+    const day = dateObject.getDate();
+    const dayOfWeek = dateObject.getDay();
+
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayName = daysOfWeek[dayOfWeek];
+
+   return `${dayName}, ${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
+
+    }
+    
+  },
+  
 }
 </script>
 
