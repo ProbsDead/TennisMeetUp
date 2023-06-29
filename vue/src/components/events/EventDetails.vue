ae38b9b190ff8bedb08002b46ac6502abb8d4e90
Kate Kim <bkkatekim1@gmail.com>
2023-06-29 18:47:27 -0400

    event details page, starting on matches

--- vue/src/components/events/EventDetails.vue
+++ vue/src/components/events/EventDetails.vue
@@ -5,30 +5,60 @@
       <div v-if="attending" class="button">Attending</div>
       <div v-else class="button btn-join">Join Event</div>
     </div>
+    <div class="group icon-txt">
+      <i class="bx bxs-group"></i>
+      <span>Hosted by {{ eventGroup.group_name }}</span>
+    </div>
     <section class="top">
       <hr />
       <div class="event-info">
-        <div class="group">
-          <i class="bx bxs-group"></i>Hosted by {{ eventGroup.group_name }}
-        </div>
         <h3>When and Where</h3>
-        <div class="when">
-          <i class="bx bx-calendar-star"></i> Date and Time
-          <div>{{ formattedDate(event.start_time) }}</div>
-          <div>
-            {{ formattedTime(event.start_time) }} -
-            {{ formattedTime(event.end_time) }}
+        <div class="when-where">
+          <div class="when">
+            <div classs="icon-txt">
+              <i class="bx bx-calendar-star"></i> <span>Date and Time</span>
+            </div>
+
+            <div>{{ formattedDate(event.start_time) }}</div>
+            <div>
+              {{ formattedTime(event.start_time) }} -
+              {{ formattedTime(event.end_time) }}
+            </div>
+          </div>
+          <div class="where">
+            <div class="icon-txt">
+              <i class="bx bx-map" /> <span>Location</span>
+            </div>
+
+            <div>{{ event.location }}</div>
           </div>
         </div>
 
-        <div class="where"><i class="bx bx-map" /> {{ event.location }}</div>
         <h3>About this event</h3>
         <div class="about">{{ event.description }}</div>
-        <hr />
       </div>
+    
     </section>
     <section class="bottom">
-        <h3>Attending ({{players.length}})</h3>
+      <div class="attending"
+      >
+        <h3>Attending ({{ players.length }})</h3>
+          <hr />
+        <div class="players">
+          <span class="player" v-for="player in players" :key="player.id"
+            >{{ player.first_name }} {{ player.last_name }}</span
+          >
+        </div>
+      </div>
+    
+      <div class="matches">
+        <h3>Matches</h3>  <hr />
+        <div v-if="!eventCompleted">Input match scores once the event is complete to track players' progress!</div>
+        <div v-else>
+          <match-input-form></match-input-form>
+          <match-display></match-display>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -36,7 +66,13 @@
 <script>
 import EventService from "../../services/EventService";
 import GroupService from "../../services/GroupService";
+import MatchInputForm from './MatchInputForm.vue';
+import MatchDisplay from "./MatchDisplay.vue";
+
 export default {
+  components:{
+    MatchInputForm, MatchDisplay
+  },
   data() {
     return {
       event: {},
@@ -45,10 +81,20 @@ export default {
       attending: false,
     };
   },
+  computed: {
+    eventCompleted() {
+      // event hasn't been completed yet as of current date/time
+      if (new Date(this.event.end_time) < new Date()) return true;
+      else return false;
+    }
+  },
   created() {
+     
     EventService.getEventDetails(this.$route.params.eventId)
       .then((response) => {
         this.event = response.data;
+           
+    console.log(new Date());
       })
       .catch((err) => {
         this.handleError(err);
@@ -71,6 +117,7 @@ export default {
         this.eventGroup = response.data;
       }
     );
+ 
   },
   methods: {
     handleError(error) {
@@ -131,11 +178,25 @@ export default {
 .top {
   background-color: rgb(251, 249, 197);
 }
+
+.event-info,
+.bottom {
+  margin-left: 10px;
+  padding: 5px 10px 5px;
+}
 h1 {
   display: inline;
   margin-right: 150px;
 }
 
+h3 {
+  color: #17665d;
+  margin: 10px 0px 5px;
+}
+
+i {
+  font-size: 1.6rem;
+}
 hr {
   border: 1px solid rgb(232, 229, 229);
 }
@@ -146,7 +207,8 @@ hr {
   border-width: 0;
   color: #333333;
   display: inline-block;
-  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
   font-size: 14px;
   font-weight: 500;
   line-height: 20px;
@@ -159,7 +221,39 @@ hr {
   white-space: nowrap;
 }
 
-.btn-join{
-      cursor: pointer;
+.btn-join {
+  cursor: pointer;
+}
+
+div.when-where {
+  display: flex;
+  gap: 180px;
+  margin-bottom: 25px;
+}
+
+div.icon-txt {
+  display: flex;
+  align-items: center;
+}
+
+div.attending, div.matches{
+  margin-bottom: 40px;
+}
+div.players {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 50px;
+}
+
+div.players span.player {
+  background-color: rgb(244, 248, 251);
+  text-align: center;
+  border-radius: 30px;
+}
+
+@media print {
+  .noprint {
+    display: none;
+  }
 }
 </style>
---
+++ vue/src/components/events/MatchInputForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form action="">
+        <input type="text"> 
+        
+        <button> Submit </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
